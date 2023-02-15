89e19a043b388536cef85283581a958a2320a52c
Angel Romero <mrangelrcd@gmail.com>
2023-02-15 21:54:31 +0100

    created state for operation in display

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Display from './Display'
 import ButtonPanel from './ButtonPanel'
 
 function Calculator() {
+  const [operation, setOperation] = useState('')
+
   return (
     <div className='calculator-body'>
-      <Display />
+      <Display operation={operation} />
       <ButtonPanel />
     </div>
   )
--- components/Display.jsx
+++ components/Display.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Display() {
+function Display( {operation} ) {
   return (
-    <div className='display'></div>
+    <div className='display'>
+      <p className='operation'>{operation}</p>
+      <p className='result'></p>
+    </div>
   )
 }
 
